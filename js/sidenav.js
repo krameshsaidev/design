3d08b85532f2caf4a58949465cd1a8037c2a69ad
K Ramesh Sai <krameshsai@gmail.com>
2022-02-21 23:36:40 +0530

    added styles

--- js/sidenav.js
+++ js/sidenav.js
@@ -21,7 +21,7 @@ $('#navbarSupportedContent .nav-link').on('click', function () {
             Track: ['Find Previous Eligibility Response', 'Find Previous Referral/Auth/Inquiry', 'Upload CSV Elig Batch', 'Find Patient Eligibility']
         };
     }
-    else if (menuitem.toLowerCase() == 'claims and tracking') {
+    else if (menuitem.toLowerCase() == 'claims&tracking') {
         var menuheaders = ['Submit_Claims', 'Track_Claims', 'Manage_Claim_Status_Inquiry'];
         var menuitems = {
             Submit_Claims: ['Upload File', 'Create New Claim', 'Create Secondary Claim', 'Copy From Existing Claim', 'Review Claim Workspace', 'Manage Patient List', 'Copy Rejection For Resub'],
@@ -29,28 +29,28 @@ $('#navbarSupportedContent .nav-link').on('click', function () {
             Manage_Claim_Status_Inquiry: ['Real-Time Claim Status Inquiry', 'Find Previous CSI Response', 'Upload CSI CSV File', 'Find CSV CSI File']
         };
     }
-    else if (menuitem.toLowerCase() == 'rejections and denials') {
+    else if (menuitem.toLowerCase() == 'rejections&denials') {
         var menuheaders = ['Manage_Rejected_Claims', 'Track_Denied_Remits_/ Eobs'];
         var menuitems = {
             Manage_Rejected_Claims: ['Find All Rejected Claims', 'Find CH Rejected Claims', 'Find Payer Rejected Claims'],
             'Track_Denied_Remits_/ Eobs': ['Find Payer Reported Denials', 'Find Zero Paid,No Patient Responsibility']
         };
     }
-    else if (menuitem.toLowerCase() == 'payments and billing') {
+    else if (menuitem.toLowerCase() == 'payments&billing') {
         var menuheaders = ['Manage_Remits_/ Eobs', 'Manage_Patient_Billing'];
         var menuitems = {
             'Manage_Remits_/ Eobs': ['Find Remittance/EOB/Check', 'Find Remit File/Batch of Remits', 'Find Claims With Payments', 'Find Claims Without Payments'],
             Manage_Patient_Billing: ['Find ZeroPaid,With Patient Responsibility', 'Take Patients Payment', 'Find Payments Taken', 'Go To TransFirtst Tx Central', 'Manage Patient Statements', 'Go to PaymentManager']
         };
     }
-    else if (menuitem.toLowerCase() == 'reporting and metrics') {
+    else if (menuitem.toLowerCase() == 'reporting&metrics') {
         var menuheaders = ['Key_Performance_Reports', 'MailBox'];
         var menuitems = {
             Key_Performance_Reports: ['Performance Dashboard', 'Benchmarking', 'Processed Claims Report', 'Processed Remittance Report', 'Processed Paper Claims Report'],
             MailBox: ['Manage Claim Submission Reports', 'Manage Archieved Claim Submission Reports', 'Manage Remittance Files', 'Manage Scheduled Portal Reports']
         };
     }
-    else if (menuitem.toLowerCase() == 'tools and setup') {
+    else if (menuitem.toLowerCase() == 'tools&setup') {
         var menuheaders = ['My_User_Settings', 'Portal_Administration', 'Enrollment', '5010_/ ICD10_Tester'];
         var menuitems = {
             My_User_Settings: ['My Default Profile', 'My Default Service', 'My User Information', 'My Password Reset Q and A'],
